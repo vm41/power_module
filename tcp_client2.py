5b7f122f1278b17325408896c04a04d3e9925c23
Jayant <jayantjnp@gmail.com>
2017-12-20 16:54:53 -0500

    craeted tcp_client2.py in object oriented model

---
+++ tcp_client2.py
@@ -0,0 +1,47 @@
+from constants import *
+import socket
+import sys
+
+class tcpClient(object):
+
+	def __init__(self, ipAddress):
+		self.TCP_IP = ipAddress
+		self.TCP_PORT = PWR_PORT # from constants.py
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.connect((self.TCP_IP, self.TCP_PORT))
+		print 'connected to the server'
+		print 'now you can use commands: S F E M# Q'
+		print '----------------------------------'
+		self.MESSAGE = ''
+		sys.stdout.flush()
+		while(True):
+			try:
+				sys.stdout.write('Enter command: ')
+				sys.stdout.flush()
+				self.MESSAGE = raw_input()
+				if self.MESSAGE == 'Q':
+					print 'exiting'
+					sys.stdout.flush()
+					break
+				self.command = bytearray()
+				if self.MESSAGE == 'S':
+					self.command.append(PWR_Command.PWR_START)
+				if self.MESSAGE == 'F':
+					self.command.append(PWR_Command.PWR_STOP)
+				if self.MESSAGE == 'E':
+					self.command.append(PWR_Command.PWR_EVENT)
+				if self.MESSAGE.startswith('M'):
+					self.command.append(PWR_Command.PWR_MARK)
+					self.number = int(self.MESSAGE[1:])
+					self.command.append(hex2bytes(int2hex(self.number)))
+				for i in PACKET_END:
+					self.command.append(i)
+				self.s.send(self.command)
+			except:
+				print 'program stopped'
+				sys.stdout.flush()
+				break
+
+		self.s.close()
+
+newClient = tcpClient('127.0.0.1')#instaciating the class object
