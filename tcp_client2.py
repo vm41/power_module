0247b6c0590b920758a0058f8d1db68b6a820694
Jayant <jayantjnp@gmail.com>
2018-01-27 21:33:28 -0500

    added summary

--- tcp_client2.py
+++ tcp_client2.py
@@ -19,16 +19,16 @@ class tcpClient(object):
 				sys.stdout.write('Enter command: ')
 				sys.stdout.flush()
 				self.MESSAGE = raw_input()
-				if self.MESSAGE == 'Q':
+				if self.MESSAGE == 'Q' || self.MESSAGE == 'q':
 					print 'exiting'
 					sys.stdout.flush()
 					break
 				self.command = bytearray()
-				if self.MESSAGE == 'S':
+				if self.MESSAGE == 'S' || self.MESSAGE == 's':
 					self.command.append(PWR_Command.PWR_START)
-				if self.MESSAGE == 'F':
+				if self.MESSAGE == 'F' || self.MESSAGE == 'f':
 					self.command.append(PWR_Command.PWR_STOP)
-				if self.MESSAGE == 'E':
+				if self.MESSAGE == 'E' || self.MESSAGE == 'E':
 					self.command.append(PWR_Command.PWR_EVENT)
 				if self.MESSAGE.startswith('M'):
 					self.command.append(PWR_Command.PWR_MARK)
