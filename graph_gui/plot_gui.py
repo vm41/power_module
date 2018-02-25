3433c81a82efc12100539dd2bd5955fc3672133f
Jayant Solanki <jayantjnp@gmail.com>
2018-02-25 00:06:14 -0500

    pre liminary schematic for current sensor

---
+++ graph_gui/plot_gui.py
@@ -0,0 +1,14 @@
+####################plot_gui.py##############################
+# This script will provide dialog box for the graph 
+# plotting configuration which will be used for 
+# generating all.txt file
+#
+#
+#
+#
+#
+#############################################################
+import Tkinter as tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
