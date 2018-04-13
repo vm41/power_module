d473d11e308aefe6bdec8fc4352ddc553a4dd7c4
PeterVanNostrand <petermvannostrand@gmail.com>
2018-04-13 00:13:44 -0400

    Implemented object oriented approach to ADC reading
    Created a python file called "ADC128D818.py" which contains a class and all neccessary constants to create, initialize, and read from the ADC as its own object.  The measure.py code was updated to utilize this object in its logging.  I2C functionality was also abstracted into its own file and supports the FT232H breakout board, but will likely need editing before use on the Raspberry Pi

---
+++ measurement_scripts/v1.9 - Object Oriented ADC/constants.py
@@ -0,0 +1,37 @@
+class PWR_Command:
+	PWR_START = 0
+	PWR_STOP = 1
+	PWR_EVENT = 2
+	PWR_MARK = 3
+	PWR_INFO = 4
+
+class EVENT_TYPE:
+	MEASUREMENT = 1
+	EVENT = 2
+	MARK = 3
+	INFO = 4
+
+class PROGRAM_MODE:
+    PI = 0
+    PC = 1
+    PC_SIMULATE_DATA = 2
+
+MODE_SELECT = PROGRAM_MODE.PC
+PWR_PORT = 35760
+PACKET_END = '\r\r\n\n'
+VDD = 5.0 #actual voltage on a 5V pin powering the measurement module as float
+CALIBRATION_SAMPLES = 100
+
+#LOG_BUFF_COUNT = number of readings before saving to file
+if MODE_SELECT == PROGRAM_MODE.PI:
+	LOG_BUFF_COUNT = 40 
+if MODE_SELECT == PROGRAM_MODE.PC:
+	LOG_BUFF_COUNT = 1000
+if MODE_SELECT == PROGRAM_MODE.PC_SIMULATE_DATA:
+	LOG_BUFF_COUNT = 100
+
+LOG_DIR = "current"
+
+#this string is just for information, it is printed
+#on the second line of the log file after startTime
+LOG_HEADER = "#time		#event_type	#event_data(channel&current(A), MarkNumber, ..."
